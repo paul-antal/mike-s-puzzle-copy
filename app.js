4f992550b2239eaad0dd3ff07900ac23971a0570
Paul Antal <h2opaulh2o@gmail.com>
2021-05-04 13:35:21 +0300

    Added rod boxes and segments

--- app.js
+++ app.js
@@ -1,59 +1,62 @@
 const GAMECONTAINERID = 'gameContainer'
-const SQUARE = "SQUARE";
-const TWOTALL = "TWOTALL";
-const TWOWIDE = "TWOWIDE";
-class Game{
-    constructor(renderer){
-        this.map = [[],[],[],[]]
+const SQUARE = 'SQUARE';
+const TWOTALL = 'TWOTALL';
+const TWOWIDE = 'TWOWIDE';
+const RODBOXRIGHT = 'RODBOXRIGHT';
+const RODBOXMIDDLE = 'RODBOXMIDDLE';
+const RODBOXLEFT = 'RODBOXLEFT';
+const RODBOXSEGMENTLEFT = 'RODBOXSEGMENTLEFT';
+const RODBOXSEGMENTMIDDLE = 'RODBOXSEGMENTMIDDLE';
+const RODBOXSEGMENTRIGHT = 'RODBOXSEGMENTRIGHT';
+class Game {
+    constructor(renderer) {
+        this.map = [[], [], [], []]
         this.renderer = renderer;
     }
 
-    isFree(line, column){
+    isFree(line, column) {
         return line >= 0 && line < 4 && column >= 0 && column < 5 && !this.map[line][column];
     }
 
-    clearSpace(line, column){
+    clearSpace(line, column) {
         this.map[line][column] = undefined;
     }
 
-    setSpace(box, line, column){
+    setSpace(box, line, column) {
         this.map[line][column] = box;
     }
 
-    cloneMap(){
+    cloneMap() {
         return this.map.map(line => [...line]);
     }
 
-    move(line, column, direction){
+    move(line, column, direction) {
         this.map[line][column].move(direction)
     }
 
-    draw(){
+    draw() {
         this.renderer.draw(this.map);
     }
 }
 
-class HtmlRenderer{
-    constructor(containerId){
+class HtmlRenderer {
+    constructor(containerId) {
         this.containerId = containerId;
     }
 
-    draw(map){
+    draw(map) {
         let container = document.getElementById(this.containerId);
-        container.innerHTML = "";
-        for(let i = 0; i < 4; i++){
-            for(let j = 0; j < 5; j++){
+        container.innerHTML = '';
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 5; j++) {
                 let box = map[i][j];
-                if(!box)
+                if (!box)
                     continue;
-                let className = box.getClassName(i,j);
-                if(className){
-                    let element = document.createElement("div");
+                let classNames = box.getClassNames(i, j);
+                if (classNames) {
+                    let element = document.createElement('div');
                     element.classList.add('box');
-                    if(box.isFixed){
-                        element.classList.add('fixedBox')
-                    }
-                    element.classList.add(className);
+                    element.classList.add(...classNames);
                     element.style.setProperty('--line', i);
                     element.style.setProperty('--column', j);
                     container.appendChild(element);
@@ -68,12 +71,13 @@ const DOWN = [1, 0];
 const LEFT = [0, -1];
 const RIGHT = [0, 1];
 
-function addV(coords1, coords2){
-    return coords1.map((c1,i) => c1+coords2[i]);
+function addV(coords1, coords2) {
+    return coords1.map((c1, i) => c1 + coords2[i]);
 }
 
-class Box{
-    constructor(game, line, column , isFixed = false){
+class Box {
+    constructor(game, line, column, isFixed = false) {
+        this.extraClasses = [];
         this.game = game;
         this.line = line;
         this.column = column;
@@ -84,19 +88,20 @@ class Box{
         this.initialize();
     }
 
-    initialize(){
-        this.type = SQUARE
+    initialize() {
+        this.type = SQUARE;
+        this.extraClasses.push(SQUARE)
     }
 
-    move(direction){
-        if(this.isFixed)
+    move(direction) {
+        if (this.isFixed)
             return false;
         var currentSpaces = this.getCurrentSpaces();
         var nextSpaces = currentSpaces.map(s => addV(s, direction))
         currentSpaces.forEach(space => {
             this.game.clearSpace(...space)
         })
-        if(nextSpaces.some(s => !this.game.isFree(...s))){
+        if (nextSpaces.some(s => !this.game.isFree(...s))) {
             currentSpaces.forEach(space => {
                 this.game.setSpace(this, ...space);
             });
@@ -109,43 +114,114 @@ class Box{
         this.column += direction[1];
     }
 
-    getClassName(line, column){
-        if(line == this.line && column == this.column){
-            return this.type;
+    getClassNames(line, column) {
+        if (line != this.line || column != this.column) {
+            return
         }
+        let classNames = [this.type];
+        if (this.isFixed) {
+            classNames.push('fixedBox');
+        }
+        classNames.push(...this.extraClasses);
+        return classNames;
     }
 
-    getCurrentSpaces(){
+    getCurrentSpaces() {
         return [[this.line, this.column]];
     }
 }
 
-class BoxTwoTall extends Box{
-    initialize(){
+class BoxTwoTall extends Box {
+    initialize() {
         this.type = TWOTALL;
+        this.extraClasses.push(TWOTALL);
     }
-    getCurrentSpaces(){
+    getCurrentSpaces() {
         return [[this.line, this.column], [this.line + 1, this.column]];
     }
 }
 
-class BoxTwoWide extends Box{
-    initialize(){
+class BoxTwoWide extends Box {
+    initialize() {
         this.type = TWOWIDE;
     }
-    getCurrentSpaces(){
+    getCurrentSpaces() {
         return [[this.line, this.column], [this.line, this.column + 1]];
     }
 }
 
-function start(){
+class RodBox extends BoxTwoWide {
+    constructor(game, line, column, isFixed = false, hasRod = false) {
+        super(game, line, column, isFixed);
+        this.hasRod = hasRod;
+        this.extraClasses.push('RODBOX');
+    }
+
+    getClassNames(line, column) {
+        var classNames = super.getClassNames(line, column);
+        if (classNames) {
+            classNames.push('RODBOX');
+            if (this.hasRod) {
+                classNames.push('hasRod');
+            }
+        }
+        return classNames;
+    }
+}
+
+class RodBoxRight extends RodBox {
+    initialize() {
+        this.type = RODBOXRIGHT;
+        this.extraClasses.push(RODBOXRIGHT, TWOWIDE);
+    }
+}
+
+class RodBoxLeft extends RodBox {
+    initialize() {
+        this.type = RODBOXLEFT;
+        this.extraClasses.push(RODBOXLEFT, TWOWIDE);
+    }
+}
+
+class RodBoxSegment extends Box {
+    getClassNames(line, column) {
+        var classNames = super.getClassNames(line, column);
+        if (classNames) {
+            classNames.push('RODBOX');
+        }
+        return classNames;
+    }
+}
+
+class RodBoxRightSegment extends RodBoxSegment {
+    initialize(){
+        this.type = RODBOXSEGMENTRIGHT
+        this.extraClasses.push(RODBOXRIGHT, SQUARE)
+    }
+}
+
+class RodBoxLeftSegment extends RodBoxSegment {
+    initialize(){
+        this.type = RODBOXSEGMENTLEFT
+        this.extraClasses.push(RODBOXLEFT, SQUARE)
+    }
+}
+
+class RodBoxMiddleSegment extends RodBoxSegment {
+    initialize(){
+        this.type = RODBOXSEGMENTMIDDLE
+        this.extraClasses.push(RODBOXMIDDLE, SQUARE)
+    }
+}
+
+function start() {
     let renderer = new HtmlRenderer(GAMECONTAINERID);
     let game = new Game(renderer);
     initializeDragHandlers(game);
     new BoxTwoTall(game, 0, 0);
-    new BoxTwoWide(game, 0, 1, true);
-    new Box(game, 0, 3);
-    new Box(game, 0, 4);
+    new RodBoxRight(game, 0, 1, true, true);
+    new RodBoxMiddleSegment(game, 0, 3);
+    new RodBoxLeftSegment(game, 0, 4);
     new Box(game, 1, 1);
     new Box(game, 1, 4);
     new Box(game, 2, 0);
@@ -154,50 +230,50 @@ function start(){
     new BoxTwoTall(game, 2, 4);
     new Box(game, 3, 0);
     new Box(game, 3, 1);
-    new BoxTwoWide(game, 3, 2, true);
+    new RodBoxLeft(game, 3, 2, true);
     game.draw();
 }
 
-function initializeDragHandlers(game){
+function initializeDragHandlers(game) {
     let container = document.getElementById(GAMECONTAINERID);
     let currentlyDraggedElement;
     container.addEventListener('mousedown', (event) => {
-        if(event.target == container)
+        if (event.target == container)
             return;
         event.stopPropagation();
         currentlyDraggedElement = event.target;
     })
     document.addEventListener('mouseup', (event) => {
         event.stopPropagation();
-        if(!currentlyDraggedElement){
+        if (!currentlyDraggedElement) {
             return;
         }
 
         let direction = getDirection(currentlyDraggedElement, event.clientX, event.clientY);
-        if(!direction){
+        if (!direction) {
             return;
         }
         let line = getComputedStyle(currentlyDraggedElement).getPropertyValue('--line');
         let column = getComputedStyle(currentlyDraggedElement).getPropertyValue('--column');
-        game.move(line,column,direction);
+        game.move(line, column, direction);
         game.draw();
         currentlyDraggedElement = undefined;
     })
 }
 
-function getDirection(element, x, y){
+function getDirection(element, x, y) {
     var bounds = element.getBoundingClientRect()
-    var centerX = (bounds.left + bounds.right)/2;
-    var centerY = (bounds.top + bounds.bottom)/2;
-    if(Math.abs(centerX - x) < 1 && Math.abs(centerY - y) < 1)
+    var centerX = (bounds.left + bounds.right) / 2;
+    var centerY = (bounds.top + bounds.bottom) / 2;
+    if (Math.abs(centerX - x) < 1 && Math.abs(centerY - y) < 1)
         return;
-    if(Math.abs(centerX - x) > Math.abs(centerY - y)){
-        if(centerX < x)
+    if (Math.abs(centerX - x) > Math.abs(centerY - y)) {
+        if (centerX < x)
             return RIGHT;
         else
             return LEFT;
     } else {
-        if(centerY < y)
+        if (centerY < y)
             return DOWN;
         else
             return UP;
